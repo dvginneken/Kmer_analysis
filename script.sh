9ee2c187bb33c436d62a52c45160041c489a71a8
Daphne <34064919+dvginneken@users.noreply.github.com>
2022-11-01 13:01:02 +0100

    Delete script.sh

--- script.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#SBATCH --mail-type=BEGIN,END,FAIL
-#SBATCH --mail-user=M.I.vanGinneken@umcutrecht.nl
-
-#bash kmer_clustering.sh /hpc/dla_lti/dvanginneken/HaploHIV_Daphne/haplohiv4/patient4_out/ec patient4
-Rscript DensityPlot.R /hpc/dla_lti/dvanginneken/Kmer_Clustering/patient4
